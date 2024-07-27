6034548924933ccadfdc2f18f76b1f558e698ed9
jamiefayezsksk <@jimejamie76@gmail.com>
2024-07-27 20:56:43 +0800

    Update: modified the styles

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead>
+<div class="table-container">
+    <table class="books-table">
+        <thead class="column-header-container">
             <tr>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">ID</th>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">ISBN</th>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">Title</th>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">Author</th>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">Description</th>
-                <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border border-gray-300">Date Published</th>
+                <th class="column-header">ID</th>
+                <th class="column-header">ISBN</th>
+                <th class="column-header">Title</th>
+                <th class="column-header">Author</th>
+                <th class="column-header">Description</th>
+                <th class="column-header">Date Published</th>
             </tr>
         </thead>
-        <tbody class="bg-white divide-y divide-gray-300">
+        <tbody>
             @foreach ($books as $book)
-                <tr class="hover:bg-gray-50">
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->id }}</td>
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->isbn }}</td>
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->title }}</td>
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->author }}</td>
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->description }}</td>
-                    <td class="w-1/6 py-3 px-4 border border-gray-300">{{ $book->date_published }}</td>
+                <tr class="book-row">
+                    <td class="book-cell">{{ $book->id }}</td>
+                    <td class="book-cell">{{ $book->isbn }}</td>
+                    <td class="book-cell">{{ $book->title }}</td>
+                    <td class="book-cell">{{ $book->author }}</td>
+                    <td class="book-cell">{{ $book->description }}</td>
+                    <td class="book-cell">{{ $book->date_published }}</td>
                 </tr>
             @endforeach
         </tbody>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
-<header class="bg-gray-800 text-blue py-4">
-    <h1 class="text-2xl">Book List</h1>
+<header class="header">
+    <h1>Book List</h1>
 </header>
