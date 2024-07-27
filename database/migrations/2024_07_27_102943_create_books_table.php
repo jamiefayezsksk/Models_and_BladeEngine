f70da86f8680c78be1f548fe2366aa79a244b1cd
jamiefayezsksk <@jimejamie76@gmail.com>
2024-07-27 18:33:29 +0800

    Add: Database Migration

--- database/migrations/2024_07_27_102943_create_books_table.php
+++ database/migrations/2024_07_27_102943_create_books_table.php
@@ -4,24 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('isbn')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->date('date_published')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('books');
     }
-};
+}
