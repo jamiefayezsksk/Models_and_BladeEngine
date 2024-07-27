dae3687f623fb66fd80efcec7e61d07517aeb491
jamiefayezsksk <@jimejamie76@gmail.com>
2024-07-27 18:37:51 +0800

    Add: Creation of Book Factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'isbn' => $this->faker->unique()->isbn13(),
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'date_published' => $this->faker->date(),
+        ];
+    }
+}
